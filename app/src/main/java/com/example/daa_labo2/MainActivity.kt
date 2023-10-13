b3cb980a610604027057ccf6201f374bffac2cdb
Fl4gu1zowsky <flavio.sovilla@etik.com>
2023-10-13 18:07:57 +0200

    first activity

--- app/src/main/java/com/example/daa_labo2/MainActivity.kt
+++ app/src/main/java/com/example/daa_labo2/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.daa_labo2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var myTextView:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val launchActivityButton = findViewById<Button>(R.id.button)
+        launchActivityButton.setOnClickListener {
+            val i = Intent(this, NameInputActivity::class.java)
+            startActivity(i) }
     }
+
+
 }
---
+++ app/src/main/java/com/example/daa_labo2/NameInputActivity.kt
@@ -0,0 +1,11 @@
+package com.example.daa_labo2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NameInputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_name_input)
+    }
+}
