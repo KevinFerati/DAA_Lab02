5ac6bf72909693730b287c8be6bf8aaa0c6bfc0e
KevinFerati <kevin.ferati@heig-vd.ch>
2023-10-27 17:01:56 +0200

    boilerplate pour la partie 3

---
+++ app/src/main/java/com/example/daa_labo2/IdentifiableFragment.kt
@@ -0,0 +1,6 @@
+package com.example.daa_labo2
+
+import androidx.fragment.app.Fragment
+
+class IdentifiableFragment : Fragment() {
+}
---
+++ app/src/main/java/com/example/daa_labo2/MainActivityFragments2.kt
@@ -0,0 +1,11 @@
+package com.example.daa_labo2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityFragments2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_fragments2)
+    }
+}
