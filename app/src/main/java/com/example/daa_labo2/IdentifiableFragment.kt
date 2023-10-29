b70ab0fb8b12a6ec3bd94e9e6e817eb422317e56
Kevin Ferati <kevin.ferati@hes-so.ch>
2023-10-29 10:42:17 +0100

    fin du code de la partie 3 : ajout de la navigation dans les étapes

--- app/src/main/java/com/example/daa_labo2/IdentifiableFragment.kt
+++ app/src/main/java/com/example/daa_labo2/IdentifiableFragment.kt
@@ -1,6 +1,45 @@
 package com.example.daa_labo2
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
+private const val ARG_STEP_NUMBER = "step_number"
 class IdentifiableFragment : Fragment() {
+
+    private var id = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            id = it.getInt(ARG_STEP_NUMBER)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_identifiable, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.stepNumber).text = "$id"
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(step: Int) =
+            IdentifiableFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_STEP_NUMBER, step)
+                }
+            }
+    }
+
 }
--- app/src/main/java/com/example/daa_labo2/MainActivityFragments2.kt
+++ app/src/main/java/com/example/daa_labo2/MainActivityFragments2.kt
@@ -2,10 +2,30 @@ package com.example.daa_labo2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivityFragments2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_fragments2)
+
+        findViewById<Button>(R.id.close).setOnClickListener {
+            finish()
+        }
+        findViewById<Button>(R.id.back).setOnClickListener {
+            if (supportFragmentManager.backStackEntryCount == 0) {
+                finish()
+                return@setOnClickListener
+            }
+            supportFragmentManager.popBackStack()
+        }
+
+        findViewById<Button>(R.id.next).setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.settingsStepFrag, IdentifiableFragment.newInstance(supportFragmentManager.backStackEntryCount))
+                .addToBackStack(null)
+                .commit()
+        }
+
     }
 }
