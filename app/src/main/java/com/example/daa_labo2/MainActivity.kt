c2cb912c86f665aebbf203f72d0d09c18e5486ee
KevinFerati <kevin.ferati@heig-vd.ch>
2023-10-29 19:37:00 +0100

    partie 1 : sauvegarde de l'état

--- app/src/main/java/com/example/daa_labo2/MainActivity.kt
+++ app/src/main/java/com/example/daa_labo2/MainActivity.kt
@@ -1,33 +1,48 @@
 package com.example.daa_labo2
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import com.example.daa_labo2.contracts.PickNameContract
+private const val ARG_NAME = "param_name"
 
 class MainActivity : LoggedActivity() {
 
-    private lateinit var nameText: TextView;
+    private var name: String? = null
 
-
+    private lateinit var nameText: TextView
     private val nameContract = registerForActivityResult(PickNameContract()) {
-        nameText.text = "Bonjour, veuillez entrer votre nom"
+        name = it
 
-        if (!it.isNullOrBlank()) {
-            nameText.text = ("Bonjour, ${it.replaceFirstChar { it.uppercase() }} !")
-        }
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        savedInstanceState?.let {
+            name = savedInstanceState.getString(ARG_NAME, name)
+        }
         nameText = findViewById(R.id.nameText)
+
+
+
         val launchActivityButton = findViewById<Button>(R.id.editNameButton)
         launchActivityButton.setOnClickListener {
             nameContract.launch(null)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!name.isNullOrBlank()) {
+            nameText.text = "Bonjour, $name !"
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(ARG_NAME, name)
     }
 
 }
